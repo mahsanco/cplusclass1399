8728dc03983a5303088bfef99d45a30fd4a5fd94
hamidkhd <44756013+hamidkhd@users.noreply.github.com>
2020-06-14 19:47:09 +0430

    Update and rename Assignment/project/DeCompress.hpp to Assignment/project/src/DeCompress.hpp

---
+++ Assignment/project/src/DeCompress.hpp
@@ -0,0 +1,31 @@
+/**
+ * Child Class of Compressor class.
+ * Returns a decompressed version of a input string.
+ * decompresses an input string using the ... algorithm.
+ */
+
+#ifndef _DeCompress_
+#define _DeCompress_ 
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Compressor.hpp"
+
+class DeCompress : public Compressor
+{
+private:
+    std::string file_name; /// Keeps the directory name
+    std::vector <std::string> data; /// Keeps the directory data
+
+    DeCompress(std::string _file_name);
+    ~DeCompress();
+
+public:
+    std::string read_file_data(); /// Reads the data in the received directory
+    std::string decompress_file_input(); /// /// Decompresses the data in the received directory
+    void send_decompressed(); /// Sends the decompressed result
+	
+};
+
+#endif
