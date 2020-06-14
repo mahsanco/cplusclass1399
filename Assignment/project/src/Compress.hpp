4248973984bcf32fdfb610b6a4b5f3e3fbd91729
hamidkhd <44756013+hamidkhd@users.noreply.github.com>
2020-06-14 19:45:33 +0430

    Update Compress.hpp

--- Assignment/project/src/Compress.hpp
+++ Assignment/project/src/Compress.hpp
@@ -1,17 +1,30 @@
+/**
+ * Child Class of Compressor class.
+ * Returns a compressed version of a input string.
+ * Compresses an input string using the ... algorithm.
+ */
+
 #ifndef _Compress_
 #define _Compress_ 
 
 #include <iostream>
 #include <string>
+#include <vector>
 #include "Compressor.hpp"
 
 class Compress : public Compressor
 {
 private:
-    Compress(std::string filename);
+    std::string file_name; /// Keeps the directory name
+    std::vector <std::string> data; /// Keeps the directory data
+
+    Compress(std::string _file_name);
     ~Compress();
 
 public:
+    std::string read_file_data(); /// Reads the data in the received directory
+    std::string compress_file_input(); /// /// Compresses the data in the received directory
+    void send_compressed(); /// Sends the compressed result
 	
 };
 
