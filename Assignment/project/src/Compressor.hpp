200c1562d5bfca837d09ea01aae7f646362b50b7
hamidkhd <44756013+hamidkhd@users.noreply.github.com>
2020-06-14 19:49:04 +0430

    Update and rename Assignment/project/Compressor.hpp to Assignment/project/src/Compressor.hpp

---
+++ Assignment/project/src/Compressor.hpp
@@ -0,0 +1,23 @@
+/**
+ * Base Class of Compress and DeCompress classes.
+ * Returns a compressed or decompressed version of a input string.
+ * Compresses or decompresses an input string using the ... algorithm.
+ */
+
+#ifndef _Compressor_
+#define _Compressor_ 
+
+#include <iostream>
+#include <string>
+
+class Compressor
+{
+private:
+    
+public:
+    Compressor(std::string filename);
+    ~Compressor();
+	
+};
+
+#endif
